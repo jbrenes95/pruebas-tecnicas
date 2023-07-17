9e0db9912e25f15948da1f1b3c3e616218cfc196
jbrenes95 <juanjosbrenes@gmail.com>
2023-07-17 20:06:57 +0200

    add available book count

--- pruebas/01-reading-list/jbrenes95/src/app/app.component.ts
+++ pruebas/01-reading-list/jbrenes95/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonDataService } from 'src/shared/json-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jbrenes95';
+export class AppComponent implements OnInit {
+  public booksLength: number = 0;
+  constructor(private jsonDateService: JsonDataService) {}
+  ngOnInit(): void {
+    this.jsonDateService.data$.subscribe((books) => {
+      this.booksLength = books.length;
+    });
+  }
 }
