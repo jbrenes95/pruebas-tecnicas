4bf4d6730f43b8a42ebc93b93484cd00e8c62a93
jbrenes95 <juanjosbrenes@gmail.com>
2023-07-19 18:47:10 +0200

    add pipe to map objects

--- pruebas/01-reading-list/jbrenes95/src/shared/json-data.service.ts
+++ pruebas/01-reading-list/jbrenes95/src/shared/json-data.service.ts
@@ -1,50 +1,47 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Book, Library, Root } from '../models/books';
-import { StorageService } from './storage.service';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JsonDataService {
-  private dataSubject: BehaviorSubject<any> = new BehaviorSubject<any>([]);
-  public data$: Observable<any> = this.dataSubject.asObservable();
+  private booksAvailables: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  public booksAvailables$: Observable<any> =
+    this.booksAvailables.asObservable();
 
   constructor(private http: HttpClient) {
     window.addEventListener('storage', (event: any) => {
-      console.log(event);
-
       if (event.key === 'Books') {
         const newData = JSON.parse(event.newValue);
-        console.log(newData);
-
-        this.dataSubject.next(newData);
+        this.booksAvailables.next(newData);
       }
     });
   }
 
-  getDataFromJson(): void {
-    this.http.get<any>('assets/books.json').subscribe(({ library }) => {
-      const books = this.mappingBooks(library);
-      this.setBooks(books);
-    });
+  getBooksFromJson(): void {
+    this.http
+      .get<any>('assets/books.json')
+      .pipe(
+        map(({ library }: any) => {
+          return library.map(({ book }: { book: any }) => book);
+        }),
+      )
+      .subscribe((books) => {
+        this.setBooks(books);
+      });
   }
 
-  getDataObservable(): Observable<any[]> {
-    return this.dataSubject.asObservable();
-  }
-
-  mappingBooks(library: any): any {
-    return library.map(({ book }: { book: any }) => book);
+  getBooksObservable(): Observable<any[]> {
+    return this.booksAvailables$;
   }
 
   setBooks(newData: any) {
-    this.dataSubject.next(newData);
+    this.booksAvailables.next(newData);
   }
 
-  updateData(newData: any): void {
+  updateBook(newData: any): void {
     localStorage.setItem('Books', JSON.stringify(newData));
-    this.dataSubject.next(newData);
+    this.booksAvailables.next(newData);
   }
 }
