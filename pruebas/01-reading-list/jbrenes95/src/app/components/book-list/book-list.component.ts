bcb705847aa55677da181d3b37058c39a8bab890
jbrenes95 <juanjosbrenes@gmail.com>
2023-07-19 18:48:21 +0200

    start book list

--- pruebas/01-reading-list/jbrenes95/src/app/components/book-list/book-list.component.ts
+++ pruebas/01-reading-list/jbrenes95/src/app/components/book-list/book-list.component.ts
@@ -8,28 +8,4 @@ import { JsonDataService } from 'src/shared/json-data.service';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss'],
 })
-export class BookListComponent implements OnInit {
-  private bookList: Subscription = new Subscription();
-  public library: Book[] = [];
-  constructor(private jsonDateService: JsonDataService) {}
-
-  ngOnInit(): void {
-    this.jsonDateService.getDataFromJson();
-    this.bookList = this.jsonDateService
-      .getDataObservable()
-      .subscribe((books) => {
-        this.library = books;
-      });
-  }
-
-  deleteLibraryBook(event: string) {
-    const filterBooks = this.library.filter((books) => books.ISBN != event);
-    console.log(filterBooks);
-
-    this.jsonDateService.updateData(filterBooks);
-  }
-
-  ngOnDestroy(): void {
-    this.bookList.unsubscribe();
-  }
-}
+export class BookListComponent {}
