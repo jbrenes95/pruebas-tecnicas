93629397a05e51397ee84a9a46c5ef3402f5d340
jbrenes95 <juanjosbrenes@gmail.com>
2023-07-19 15:41:28 +0200

    synchronization between tabs

--- pruebas/01-reading-list/jbrenes95/src/app/components/book-list/book-list.component.ts
+++ pruebas/01-reading-list/jbrenes95/src/app/components/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Book } from 'src/models/books';
 import { JsonDataService } from 'src/shared/json-data.service';
 
@@ -8,13 +9,27 @@ import { JsonDataService } from 'src/shared/json-data.service';
   styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
+  private bookList: Subscription = new Subscription();
   public library: Book[] = [];
   constructor(private jsonDateService: JsonDataService) {}
 
   ngOnInit(): void {
     this.jsonDateService.getDataFromJson();
-    this.jsonDateService.data$.subscribe((books) => {
-      this.library = books;
-    });
+    this.bookList = this.jsonDateService
+      .getDataObservable()
+      .subscribe((books) => {
+        this.library = books;
+      });
+  }
+
+  deleteLibraryBook(event: string) {
+    const filterBooks = this.library.filter((books) => books.ISBN != event);
+    console.log(filterBooks);
+
+    this.jsonDateService.updateData(filterBooks);
+  }
+
+  ngOnDestroy(): void {
+    this.bookList.unsubscribe();
   }
 }
--- pruebas/01-reading-list/jbrenes95/src/shared/json-data.service.ts
+++ pruebas/01-reading-list/jbrenes95/src/shared/json-data.service.ts
@@ -2,28 +2,49 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Book, Library, Root } from '../models/books';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JsonDataService {
-  constructor(private http: HttpClient) {}
-
   private dataSubject: BehaviorSubject<any> = new BehaviorSubject<any>([]);
   public data$: Observable<any> = this.dataSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    window.addEventListener('storage', (event: any) => {
+      console.log(event);
+
+      if (event.key === 'Books') {
+        const newData = JSON.parse(event.newValue);
+        console.log(newData);
+
+        this.dataSubject.next(newData);
+      }
+    });
+  }
 
   getDataFromJson(): void {
     this.http.get<any>('assets/books.json').subscribe(({ library }) => {
       const books = this.mappingBooks(library);
-      this.updateData(books);
+      this.setBooks(books);
     });
+  }
+
+  getDataObservable(): Observable<any[]> {
+    return this.dataSubject.asObservable();
   }
 
   mappingBooks(library: any): any {
     return library.map(({ book }: { book: any }) => book);
   }
 
+  setBooks(newData: any) {
+    this.dataSubject.next(newData);
+  }
+
   updateData(newData: any): void {
+    localStorage.setItem('Books', JSON.stringify(newData));
     this.dataSubject.next(newData);
   }
 }
